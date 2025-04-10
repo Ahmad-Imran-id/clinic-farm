8e1377dca082cf44623ddd17ee929cf350abf41a
Ahmad-Imran-id <imahmad302a@gmail.com>
2025-04-10 18:59:07 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,32 +6,18 @@ import Dashboard from "./Dashboard";
 import PatientManagement from './components/PatientManagement';
 import Inventory from "./pages/Inventory";
 
-<Routes>
-  {/* other routes */}
-  <Route path="/inventory" element={<Inventory />} />
-</Routes>
-
-
-function App() {
-  return (
-    <div>
-      <PatientManagement />
-    </div>
-  );
-}
-
-
 function App() {
   return (
     <Router>
       <div className="App">
         <nav>
-          <Link to="/login">Login</Link> | <Link to="/signup">Sign Up</Link> | <Link to="/dashboard">Dashboard</Link>
+          <Link to="/login">Login</Link> | <Link to="/signup">Sign Up</Link> | <Link to="/dashboard">Dashboard</Link> | <Link to="/inventory">Inventory</Link>
         </nav>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/inventory" element={<Inventory />} />
         </Routes>
       </div>
     </Router>
