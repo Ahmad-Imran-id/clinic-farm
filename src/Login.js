e8e75b76ed9b2c9060aca684a81d9399f2a0ee3e
Ahmad-Imran-id <imahmad302a@gmail.com>
2025-04-10 18:18:54 +0500

    Create Login.js

---
+++ src/Login.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { auth } from "./firebase-config";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/dashboard");
+    } catch (err) {
+      alert("Login Failed: " + err.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
