e0e90cc92e41a2a6f72c6fee6e648c4e4f9728b6
Ahmad-Imran-id <imahmad302a@gmail.com>
2025-04-11 16:28:35 +0500

    Update Billing.js

--- src/pages/Billing.js
+++ src/pages/Billing.js
@@ -43,6 +43,18 @@ const Billing = () => {
     setSuggestions([]);
   };
 
+  const [invoiceNumber, setInvoiceNumber] = useState('');
+
+useEffect(() => {
+  const generateInvoiceNumber = () => {
+    const datePart = new Date().toISOString().slice(0, 10).replace(/-/g, '');
+    const randomPart = Math.floor(1000 + Math.random() * 9000);
+    return `INV-${datePart}-${randomPart}`;
+  };
+  setInvoiceNumber(generateInvoiceNumber());
+}, []);
+
+  
   const handleCheckout = async () => {
     await addDoc(collection(db, 'sales'), {
       products: cart,
