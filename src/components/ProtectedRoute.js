06b22c99766977ec944ab1a71dc9c0f5da726ab9
Ahmad-Imran-id <imahmad302a@gmail.com>
2025-04-13 19:34:18 +0500

    Update ProtectedRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -3,8 +3,10 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth"; // custom hook we’ll create
 
 const ProtectedRoute = ({ children, allowedRoles }) => {
-  const { currentUser, role } = useAuth();
+  const { currentUser, role, loading } = useAuth();
 
+  // Prevent route from redirecting while loading
+  if (loading) return <div>Loading...</div>; // or any loading indicator
   if (!currentUser) return <Navigate to="/login" />;
   if (!allowedRoles.includes(role)) return <Navigate to="/unauthorized" />;
 
