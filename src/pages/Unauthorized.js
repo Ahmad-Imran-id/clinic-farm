75c7908a4130eb0bdeba327aafd9b709fcc992a5
Ahmad-Imran-id <imahmad302a@gmail.com>
2025-04-13 18:50:08 +0500

    Create Unauthorized.js

---
+++ src/pages/Unauthorized.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Unauthorized = () => {
+  return (
+    <div style={{ padding: "20px", color: "red" }}>
+      <h2>403 - Unauthorized</h2>
+      <p>You do not have permission to access this page.</p>
+    </div>
+  );
+};
+
+export default Unauthorized;
